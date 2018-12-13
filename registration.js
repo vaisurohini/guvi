e0daba07d7ee622db303a31321c272075a6200ed
vaisurohini <45839323+vaisurohini@users.noreply.github.com>
2018-12-13 14:05:53 +0530

    Update and rename style.css to registration.js

---
+++ registration.js
@@ -0,0 +1,36 @@
+register validation.js
+x=document.get element ByID("email").value;
+if(x=="){
+alert("email must be filled");
+return false;
+}
+x=document.get element ByID("password")value;
+if(x=="){
+alert("password must be filled");
+return false;
+}
+x=document.get element ByID("user name")value;
+if(x=="){
+alert("user name must be filled");
+return false;
+}
+x=document.get element ByID("register number")value;
+if(x=="){
+alert("register number must be filled");
+return false;
+}
+x=document.get element ByID("college name")value;
+if(x=="){
+alert("college name must be filled");
+return false;
+}
+x=document.get element ByID(" brange")value;
+if(x=="){
+alert("brange must be filled");
+return false;
+}
+x=document.get element ByID("degree ")value;
+if(x=="){
+alert("degree must be filled");
+return false;
+}
