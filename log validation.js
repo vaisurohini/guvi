9faa5a45dfd2c431700092c4e7fdbe0228515c46
vaisurohini <45839323+vaisurohini@users.noreply.github.com>
2018-12-13 14:11:58 +0530

    Create log validation.js

---
+++ log validation.js
@@ -0,0 +1,11 @@
+log validation.js
+x=document.get element ByID("email").value;
+if(x=="){
+alert("email must be filled");
+return false;
+}
+x=document.get element ByID("password")value;
+if(x=="){
+alert("password must be filled");
+return false;
+}
